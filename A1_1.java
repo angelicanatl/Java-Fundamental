29d7938ceeded5b5e0534ef9a30d2733096ee2d1
Angelica Natalie <135089209+angelicanatl@users.noreply.github.com>
2024-04-05 23:02:56 +0700

    Assigment 1
    Session 1 and 2

---
+++ A1_1.java
@@ -0,0 +1,27 @@
+//Angelica Natalie
+//Assignment 1, Session 1
+
+public class A1_1 {
+    public static void main(String[] args) {
+        // 1
+        int a = 2;
+        int b = 3;
+
+        System.out.println("Hasil penjumlahan a dan b = " + (a + b));
+
+        // 2
+        String namaDepan = "Angelica";
+        String namaTengah = "Na";
+        String namaBelakang = "Natalie";
+        int umur = 21;
+        String domisili = "Bandung";
+
+        System.out.printf("Halo, nama saya %s %s %s, umur saya %d tahun. Saya berdomisili di kota %s. %n", namaDepan, namaTengah, namaBelakang, umur, domisili);
+
+        // 3
+        double c = 8.0;
+        double d = 10.5;
+
+        System.out.println("c<d: " + (c < d));
+    }
+}
